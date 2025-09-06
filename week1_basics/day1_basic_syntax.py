76a415a02c582753d8e5b6d5f9c41fa23a14bc4f
arunkumar-02-dev <arunkumar.off210@gmail.com>
2025-09-06 20:49:39 +0530

    Day 1: Hello World & Input/Output

---
+++ week1_basics/day1_basic_syntax.py
@@ -0,0 +1,7 @@
+# Day 1: Basic Syntax
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello", name, "you are", age, "years old")
+print(f"Hello {name}, you are {age} years old")
